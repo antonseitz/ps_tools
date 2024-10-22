1bf2aed6a83eabeb8f1c20766e064ababd05315a
antonseitz <aseitz@puckit.de>
2024-10-22 08:57:16 +0200

    Update ps_tools.ps1

--- ps_tools.ps1
+++ ps_tools.ps1
@@ -47,7 +47,7 @@ $run_this_exe_after_unzip='TreeSizeFree.exe'
 }
 
 "12" { $url='https://github.com/windirstat/windirstat/releases/download/release%2Fv2.0.1/WinDirStat-x64.msi'
-
+}
 
 
 "13" { $url='https://github.com/WinMerge/winmerge/releases/download/v2.16.42.1/WinMerge-2.16.42.1-x64-Setup.exe' }
